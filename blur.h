ea4793cd74401fb190a72af5f5d060a502e8dde0
Filip Jankowski <jankowski.filip531@gmail.com>
2019-04-07 22:01:42 +0200

    fixed formatting

--- blur.h
+++ blur.h
@@ -3,7 +3,6 @@
 #ifndef __INCLUDE_BLUR_H__
 #define __INCLUDE_BLUR_H__
 
-void gaussBlur_4(uint8_t* scl, uint8_t* tcl, int w, int h, int r, int bpp);
+void fast_gauss_blur(uint8_t *scl, uint8_t *tcl, int w, int h, int r, int bpp);
 
 #endif
-
--- test/test.c
+++ test/test.c
@@ -9,18 +9,19 @@
 
 #define CHANNEL_NUM 3
 
-int main(int argc, char **argv) {
+int main(int argc, char **argv)
+{
 	int width, height, bpp;
 	int r = 5;
 
-	uint8_t* rgb_image = stbi_load(argv[1], &width, &height, &bpp, 3);
+	uint8_t *rgb_image = stbi_load(argv[1], &width, &height, &bpp, 3);
 
-	uint8_t* rgb_image_w;
-	rgb_image_w = malloc(width*height*CHANNEL_NUM);
+	uint8_t *rgb_image_w;
+	rgb_image_w = malloc(width * height * CHANNEL_NUM);
 
-	gaussBlur_4(rgb_image, rgb_image_w, width, height, r, bpp);
+	fast_gauss_blur(rgb_image, rgb_image_w, width, height, r, bpp);
 
-	stbi_write_png(argv[2], width, height, CHANNEL_NUM, rgb_image_w, width*CHANNEL_NUM);
+	stbi_write_png(argv[2], width, height, CHANNEL_NUM, rgb_image_w, width * CHANNEL_NUM);
 
 	stbi_image_free(rgb_image);
 
