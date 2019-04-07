04fc1ef7d9b7121215b73f61b80b8fbe734a1c46
Filip Jankowski <jankowski.filip531@gmail.com>
2019-04-07 21:40:26 +0200

    initial

---
+++ test/test.c
@@ -0,0 +1,28 @@
+#include <blur.h>
+#include <stdint.h>
+
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
+
+#define STB_IMAGE_WRITE_IMPLEMENTATION
+#include "stb_image_write.h"
+
+#define CHANNEL_NUM 3
+
+int main(int argc, char **argv) {
+	int width, height, bpp;
+	int r = 5;
+
+	uint8_t* rgb_image = stbi_load(argv[1], &width, &height, &bpp, 3);
+
+	uint8_t* rgb_image_w;
+	rgb_image_w = malloc(width*height*CHANNEL_NUM);
+
+	gaussBlur_4(rgb_image, rgb_image_w, width, height, r, bpp);
+
+	stbi_write_png(argv[2], width, height, CHANNEL_NUM, rgb_image_w, width*CHANNEL_NUM);
+
+	stbi_image_free(rgb_image);
+
+	return 0;
+}
