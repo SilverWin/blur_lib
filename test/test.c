568b7edab1947b8f8eec91b822a995dc4a104e98
Oldes <oldes.huhuman@gmail.com>
2021-06-29 12:27:37 +0200

    FIX: basic input assertion to avoid crash when input is not specified

--- test/test.c
+++ test/test.c
@@ -13,15 +13,24 @@ int main(int argc, char **argv)
 {
 	int width, height, bpp;
 	int r = 5;
+	char* input  = argv[1];
+	char* output = argv[2];
 
-	uint8_t *rgb_image = stbi_load(argv[1], &width, &height, &bpp, 3);
+	if (input == NULL) input = "in.png";
+	if (output == NULL) output = "out.png";
+
+	uint8_t *rgb_image = stbi_load(input, &width, &height, &bpp, 3);
+	if (rgb_image == NULL) {
+		printf("Cannot open input PNG file: %s\n", input);
+		exit(1);
+	}
 
 	uint8_t *rgb_image_w;
-	rgb_image_w = malloc(width * height * CHANNEL_NUM);
+	rgb_image_w = malloc((size_t)width * height * CHANNEL_NUM);
 
 	fast_gauss_blur(rgb_image, rgb_image_w, width, height, r, bpp);
 
-	stbi_write_png(argv[2], width, height, CHANNEL_NUM, rgb_image_w, width * CHANNEL_NUM);
+	stbi_write_png(output, width, height, CHANNEL_NUM, rgb_image_w, width * CHANNEL_NUM);
 
 	stbi_image_free(rgb_image);
 
